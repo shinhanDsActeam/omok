2c044a0e97f1487cff88b6547f7eee65939cb62e
Your Name <you@example.com>
2025-05-22 12:20:26 +0900

    feat : roomcreate update
    방 생성 기능 업데이트

--- src/main/util/DBUtil.java
+++ src/main/util/DBUtil.java
@@ -1,4 +1,14 @@
 package main.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DBUtil {
+    public static Connection getConnection() throws Exception {
+        String url = "jdbc:mysql://127.0.0.1:3307/omock";
+        String user = "root"; // 사용자명
+        String password = "root1234"; // 비밀번호
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(url, user, password);
+    }
 }
