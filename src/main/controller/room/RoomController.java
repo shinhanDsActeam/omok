52089683e22452adb7afe79170715542d2e4801f
Your Name <you@example.com>
2025-05-22 12:17:45 +0900

    feat : roomcreate update
    방 생성 기능 업데이트

--- src/main/controller/room/RoomController.java
+++ src/main/controller/room/RoomController.java
@@ -4,8 +4,6 @@ import main.db.RoomDAO;
 import main.model.Room;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
