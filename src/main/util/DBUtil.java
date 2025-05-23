60f72b1a49caeefc398d52c27947896914a105ed
Mouon <ahemsapsldk@gmail.com>
2025-05-23 11:48:09 +0900

    feat : 게임 참여 구현 완료

--- src/main/util/DBUtil.java
+++ src/main/util/DBUtil.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 
 public class DBUtil {
     public static Connection getConnection() throws Exception {
-        String url = "jdbc:mysql://127.0.0.1:3308/omock";
+        String url = "jdbc:mysql://127.0.0.1:3306/omock";
         String user = "root"; // 사용자명
-        String password = "1234"; // 비밀번호
+        String password = ""; // 비밀번호
         Class.forName("com.mysql.cj.jdbc.Driver");
         return DriverManager.getConnection(url, user, password);
     }
